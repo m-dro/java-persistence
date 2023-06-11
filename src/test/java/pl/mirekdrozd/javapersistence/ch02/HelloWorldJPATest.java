fa1eee4d37e3411854216cb631d395b7cfe59ea6
Mirek Drozd <mirek.drozd@mail.com>
2023-06-11 14:11:31 +0200

    complete first exercise

---
+++ src/test/java/pl/mirekdrozd/javapersistence/ch02/HelloWorldJPATest.java
@@ -0,0 +1,52 @@
+package pl.mirekdrozd.javapersistence.ch02;
+
+import org.junit.jupiter.api.Test;
+import pl.mirekdrozd.javapersistence.ch02.Message;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class HelloWorldJPATest {
+
+    @Test
+    public void storeLoadMessage(){
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("ch02");
+
+        try {
+            EntityManager em = emf.createEntityManager();
+            em.getTransaction().begin();
+
+            Message message = new Message();
+            message.setText("Hello World!");
+
+            em.persist(message);
+
+            em.getTransaction().commit();
+
+            em.getTransaction().begin();
+            final List<Message> messages = em.createQuery(
+                    "select m from Message m", Message.class
+            ).getResultList();
+
+            messages.get(messages.size() -1).setText("Hello World from JPA!");
+
+            em.getTransaction().commit();
+
+            assertAll(
+                    () -> assertEquals(1, messages.size()),
+                    () -> assertEquals("Hello World from JPA!", messages.get(0).getText())
+            );
+            em.close();
+        } finally {
+            emf.close();
+        }
+    }
+
+}
